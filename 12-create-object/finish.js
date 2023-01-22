b1a181d4cd2cfdef0f0afdd4197d1f06f584716c
Kostiantyn Kondratenko <evpatyikalovrat@gmail.com>
2023-01-22 08:10:12 -0500

    js course

---
+++ 12-create-object/finish.js
@@ -0,0 +1,18 @@
+/** ЗАДАЧА 12 - Создание объекта
+ *
+ * 1. Создайте объект с тремя свойствами:
+ *  - name
+ *  - surname
+ *  - favoriteNumber
+ *
+ * 2. Выведите в консоль строку
+ * "My name is <name> <surname> and my favorite number is <favoriteNumber>"
+ */
+let obj = {
+    name1: 'kostia',
+    surname2: 'asla',
+    favoriteNumber: 4
+}
+
+
+console.log(`My name is ${obj.name1 + obj.surname2} and my favorite number is ${obj.favoriteNumber }>`)
